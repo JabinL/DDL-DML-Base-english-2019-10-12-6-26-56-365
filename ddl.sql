5bb9b25d48e3752166d7eecb79f7703d7e366bc5
Jabin <2099008974@qq.com>
2020-06-26 16:31:55 +0800

    finished

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use db_name;
 -- Create a database
-
+create db_name;
 -- Create the database of the designated character set
-
+create database db_name character set utf8;
 -- Display the creation information fo the database
-
+show create database db_name;
 -- Revise the codes of the database
-
+alter database db_name character set gb2312;
 -- Delete a database
-
+drop database db_name;
 -- **Table level**
 -- Revise table name
-
+alter table student rename stu;            
 -- Revise the field's data type
-
+alter table stu modify sex varchar(50);
 -- Revise field name
-
+alter table stu change name stu_name varchar(20) ; 
 -- Add field
-
+alter table stu add school varchar(30);
 -- Delete field
-
+alter table stu drop school;
 -- Revise the table's storage engine
-
+alter table stu engine=MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table t1 drop foreign key fk_name; 
 -- Delete a table
+drop table tb_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- Insert record
+insert into student (id,name,age,sex) values('003','wang wu','22', 'male');
 
 -- Revise record
-
+update student set age = '21' where id = 3;
 --  Delete record
-
+	delete from student where id = 3;
 -- Search record
+select * from student where id = 1;
